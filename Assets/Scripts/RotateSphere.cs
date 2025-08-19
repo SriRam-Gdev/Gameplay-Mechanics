4efd4444431f637e2bfb3cbc89ad5a50aaf3dbeb
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2025-08-19 11:36:55 +0530

    input handling

--- Assets/Scripts/RotateSphere.cs
+++ Assets/Scripts/RotateSphere.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RotateSphere : MonoBehaviour
 {
 
-    public float rotationSpeed = 30;
+    public float rotationSpeed = 50;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
