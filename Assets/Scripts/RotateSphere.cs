b270ffcc38f8b3284a3e64064021e4fcee156f7d
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2025-08-19 11:08:00 +0530

    rotation script

--- Assets/Scripts/RotateSphere.cs
+++ Assets/Scripts/RotateSphere.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class RotateSphere : MonoBehaviour
 {
+
+    public float rotationSpeed = 30;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 
     // Update is called once per frame
