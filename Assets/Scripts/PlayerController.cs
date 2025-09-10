5997b18c210bf6385d12ded75c76312ed43e9767
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2025-09-10 11:57:48 +0530

    Collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,4 +31,11 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+         if(collision.gameObject.CompareTag("Enemy") && HasPowerUp)
+         {
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + HasPowerUp);
+        }
+    }
 }
